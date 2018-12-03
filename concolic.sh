d3c51002d4d60509129d1620ce8b97ced94e23aa
ikhwan12 <ikhwan.ikhwan52@gmail.com>
2018-12-03 15:44:16 +0800

    main tool

---
+++ concolic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+klee --max-instruction-time=30. --simplify-sym-indices --max-memory=2000 --max-sym-array-size=4096 --ignore-solver-failures --only-output-states-covering-new --libc=uclibc --posix-runtime ./bin.bc --sym-args 1 3 12 --sym-files 1 128 > /dev/null
+
+
+# --ignore-solver-failures: running further even is the solver crashes
+# --optimize: remove dead code, optimize bytecode
+# --only-output-states-covering-new: minimize file output
+# --libc=uclibc --posix-runtime: support --sym-args to interface the application & external lib calls
---
+++ tool/generate.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zcov genhtml coverage.zcov crash_report/cov_report
